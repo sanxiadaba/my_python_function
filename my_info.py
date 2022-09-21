ddbadb786777ddd8f97bef25a80ee2725ab3e884
sanxiadaba <2190165626@qq.com>
2022-09-21 23:13:11 +0800

    新增info、normal文件
    分割线以及打印版本信息的函数

---
+++ my_info.py
@@ -0,0 +1,7 @@
+# 关于信息的函数
+import sys
+
+
+# 打印Python的版本
+def print_version():
+    print(sys.version)
---
+++ usage.py
@@ -0,0 +1,6 @@
+# 测试每个功能的使用
+import my_info
+import my_normal
+
+my_normal.line(100)
+my_info.print_version()
