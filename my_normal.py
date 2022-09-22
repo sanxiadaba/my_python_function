d38094acb1f293db854fac9bc9547445b1bbd277
sanxiadaba <2190165626@qq.com>
2022-09-22 18:17:26 +0800

    增加打印变量的函数

--- my_normal.py
+++ my_normal.py
@@ -1,7 +1,16 @@
 # 日常使用的函数
-# 分割线函数
+import inspect
+import re
 
 
 # 打印分割线的函数
 def line(n: int):
     print("\033[1;31;40m", (" " + str(n) + " ").center(80, "="), "\033[0m")
+
+
+# 打印变量名的函数
+def varname(var):
+    for line in inspect.getframeinfo(inspect.currentframe().f_back)[3]:
+        m = re.search(r'\bvarname\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
+        if m:
+            print(m.group(1))
--- usage.py
+++ usage.py
@@ -2,5 +2,10 @@
 import my_info
 import my_normal
 
+my_str = "hello"
+# 分割线
 my_normal.line(100)
+# 打印版本
 my_info.print_version()
+# 打印变量
+my_normal.varname(my_str)
